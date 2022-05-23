dd7b3b8fbfc079395c306c5e707975c3e068df94
masakiimagawa <93180567+masakiimagawa@users.noreply.github.com>
2022-05-23 19:44:38 +0900

    Update network.tf

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/imagawa-training/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = azurerm_subnet.subnet
+  resource_group_name = network
 }
